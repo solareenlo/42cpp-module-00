dd9fe7a5b5fa95d1bf2faf9b5935b8f8055ffa2c
solareenlo <solareenlo@gmail.com>
2021-05-21 21:54:44 +0900

    fix: ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: solareenlo <solareenlo@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 20:40:11 by solareenlo        #+#    #+#             */
-/*   Updated: 2021/05/21 21:29:28 by solareenlo       ###   ########.fr       */
+/*   Updated: 2021/05/21 21:53:29 by solareenlo       ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -15,26 +15,13 @@
 
 #define NOISE    "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
-size_t    ft_strlen(char *s) {
-    size_t    i;
-
-    if (s == NULL)
-        return (0);
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
-}
-
 int    main(int argc, char *argv[]) {
     if (argc == 1) {
         std::cout << NOISE << std::endl;
     } else {
-        for (int i = 1; i < argc; i++) {
-            size_t    len = ft_strlen(argv[i]);
-            for (size_t j = 0; j < len; j++)
+        for (int i = 1; i < argc; i++)
+            for (size_t j = 0; argv[i][j]; j++)
                 std::cout << static_cast<char>(toupper(argv[i][j]));
-        }
         std::cout << std::endl;
     }
     return (0);
