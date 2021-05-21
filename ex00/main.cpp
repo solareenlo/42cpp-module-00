f83afa436b45860c837a77db8bfb6e7d60b7d135
solareenlo <solareenlo@gmail.com>
2021-05-21 20:42:48 +0900

    fix: cpplint

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,43 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: solareenlo <solareenlo@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/21 11:14:32 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/21 11:47:34 by tayamamo         ###   ########.fr       */
-/*                                                                            */
+/*   Created: 2021/05/21 20:40:11 by solareenlo        #+#    #+#             */
+/*   Updated: 2021/05/21 20:40:25 by solareenlo       ###   ########.fr       */
+/*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-#define NOISE	"* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+#define NOISE    "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
-size_t	ft_strlen(char *s)
-{
-	size_t	i;
+size_t    ft_strlen(char *s) {
+    size_t    i;
 
-	if (s == NULL)
-		return (0);
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+    if (s == NULL)
+        return (0);
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
 }
 
-int	main(int argc, char *argv[])
-{
-	if (argc == 1)
-		std::cout << NOISE << '\n';
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			size_t	len = ft_strlen(argv[i]);
-			for (size_t j = 0; j < len; j++)
-				std::cout << (char)toupper(argv[i][j]);
-		}
-		std::cout << std::endl;
-	}
-	return (0);
+int    main(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cout << NOISE << '\n';
+    } else {
+        for (int i = 1; i < argc; i++) {
+            size_t    len = ft_strlen(argv[i]);
+            for (size_t j = 0; j < len; j++)
+                std::cout << static_cast<char>(toupper(argv[i][j]));
+        }
+        std::cout << std::endl;
+    }
+    return (0);
 }
